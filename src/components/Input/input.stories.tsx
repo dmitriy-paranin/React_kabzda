8b2dff85850273607496c29e92399cd215feefa3
Dmitriy <paranin-dmitriy@mail.ru>
2022-09-02 10:43:51 +0300

    13

--- src/components/Input/input.stories.tsx
+++ src/components/Input/input.stories.tsx
@@ -25,6 +25,30 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     const el = inputRef.current as HTMLInputElement;
     setValue(el.value);
   }
-  return <><input ref={inputRef} id={"inputId"} type="text"/><button onClick={save}>save</button> - {value}</>
+  return <><input ref={inputRef} type="text"/><button onClick={save}>save</button> - {value}</>
 };
 
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState("");
+  const onChange = (e: ChangeEvent<HTMLInputElement>)=> {setParentValue(e.currentTarget.value)};
+
+  return <input type="text" value={parentValue} onChange={onChange}/>
+};
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(true);
+  const onChange = (e: ChangeEvent<HTMLInputElement>)=> {setParentValue(e.currentTarget.checked)};
+
+  return <input type="checkbox" checked={parentValue} onChange={onChange}/>
+};
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>(undefined);
+  const onChange = (e: ChangeEvent<HTMLSelectElement>)=> {setParentValue(e.currentTarget.value)};
+
+  return <select  value={parentValue} onChange={onChange}>
+    <option value="">none</option>
+    <option value="1">Minsk</option>
+    <option value="2">Moskow</option>
+    <option value="3">Kiev</option>
+  </select>
+};
