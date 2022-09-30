25453dd36b5b6c171ad44c620fed4694c1f433cc
Dmitriy <paranin-dmitriy@mail.ru>
2022-09-30 22:13:20 +0300

    19

--- src/components/UseCollback/UseCollback.stories.tsx
+++ src/components/UseCollback/UseCollback.stories.tsx
@@ -1,16 +1,16 @@
-import React, {useMemo, useState} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 
 export default {
     title: 'React.memo demo',
 }
 
-const NewMessagesCounter = (props: {count: number}) => {
+const NewMessagesCounter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
-const UsersSecret = (props: {users: Array<string>}) => {
+const UsersSecret = (props: { users: Array<string> }) => {
     console.log("USERS")
     return <div>{
-        props.users.map((u,i)=><div key={i}>{u}</div>)
+        props.users.map((u, i) => <div key={i}>{u}</div>)
     }</div>
 }
 const Users = React.memo(UsersSecret)
@@ -25,19 +25,28 @@ export const LikeUseCollback = () => {
         return newArray
     }, [books])
 
-    const addBooks = () => {
+    const memoizedAddBook = useMemo(() => {
+        return () => {
+            console.log(books)
+            const newUsers = [...books, 'Angular ' + new Date().getTime()];
+            setBooks(newUsers)
+        }
+    }, [books]);
+
+    const memoizedAddBook2 = useCallback(() => {
+        console.log(books)
         const newUsers = [...books, 'Angular ' + new Date().getTime()];
         setBooks(newUsers)
-    }
+    }, [books]);
+
     return <>
-        <button onClick={()=>setCounter(counter + 1)}>+</button>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
         {counter}
-        <Book books={newArray} addBook={addBooks}/>
+        <Book addBook={memoizedAddBook}/>
     </>
 }
 
 type BookSecretPropsType = {
-    books: Array<string>
     addBook: () => void
 }
 
